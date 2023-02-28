bb6b40a1e108555ce23fef51a7215415d70240f1
obee58 <obee58@gmail.com>
2023-02-28 10:13:38 -0500

    this is how i'm handling the API key personally

--- script/tmdbtest-obook.py
+++ script/tmdbtest-obook.py
@@ -1,9 +1,25 @@
-import tmdbsimple
-import requests
+import json
+import sys
+import os
+import requests as req
+
+import tmdbsimple as tmdb
 
 def main():
-    #TODO code here
+    if len(sys.argv) <= 2:
+        print(f"usage: {sys.argv[0]} MOVIE_TITLE", file=sys.stderr)
 
+    with open("../tmdbKEY") as keyFile:
+        tmdb.API_KEY = keyFile.read()
+
+    searchTitle = sys.argv[1]
+
+    search = tmdb.Search()
+    response = search.movie(query=searchTitle)
+    print(response)
+
+    for t in search.results:
+        print(s)
 
 
 if __name__ == '__main__':
