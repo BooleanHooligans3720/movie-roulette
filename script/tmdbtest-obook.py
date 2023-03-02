72564ee58e17c393fee7dcc405b2016745d7855a
obee58 <obee58@gmail.com>
2023-03-02 10:48:58 -0500

    unpacking experiments

--- script/tmdbtest-obook.py
+++ script/tmdbtest-obook.py
@@ -6,20 +6,24 @@ import requests as req
 import tmdbsimple as tmdb
 
 def main():
-    if len(sys.argv) <= 2:
+    if len(sys.argv) < 2:
         print(f"usage: {sys.argv[0]} MOVIE_TITLE", file=sys.stderr)
+        exit()
 
-    with open("../tmdbKEY") as keyFile:
+    with open("script\\tmdbKEY", mode='rt') as keyFile:
         tmdb.API_KEY = keyFile.read()
 
     searchTitle = sys.argv[1]
 
     search = tmdb.Search()
     response = search.movie(query=searchTitle)
-    print(response)
+    #print(response)
 
     for t in search.results:
-        print(s)
+        print(t.keys())
+        print(json.dumps(t, indent=4))
+
+    print(response.keys())
 
 
 if __name__ == '__main__':
